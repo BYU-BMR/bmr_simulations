6b6c04d86a3b8d2d69fdc7a5eec18c32fc1e388a
Anthony Gillespie <agilles2@m8int01.fsl.byu.edu>
2017-05-18 16:11:02 -0600

    setup.sh now checks if .local/bin is already on PATH

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# This will pull all submodules and install LAMMPS
+# This script should be run inside the bmr_simulations directory
+# After running this script you should be ready to run a simulation
+
 # Update submodules
 git submodule update --init --recursive
 
 # Make a .local/bin/ directory
 mkdir -p .local/bin/
-
-
 
 # Build LAMMPS
 cd lammps/src/
@@ -24,8 +26,16 @@ cp lmp_mpi ../../.local/bin
 cd ../../.local/bin
 ln -s lmp_mpi lammpz
 
-# Add the .local/bin/ directory to your path
-echo -e '\nexport PATH="'$PWD'/.local/bin:$PATH"' >> $HOME/.bash_profile
-source $HOME/.bash_profile
+# Check if the .local/bin/ directory is not on your path
+if [[ $PATH != ?(*:)$PWD/.local/bin?(:*) ]]
+then
+	# Add the .local/bin/ directory to your path
+	echo "Adding $PWD/.local/bin to PATH"
+	echo -e '\nexport PATH="'$PWD'/.local/bin:$PATH"' >> $HOME/.bash_profile
+	source $HOME/.bash_profile
+else
+
+
+
 
 

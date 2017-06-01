9dd988c135881141d6bb685d1b292290563eb4d7
mnikpour <mnikpour@m8int01.fsl.byu.edu>
2017-06-01 16:27:51 -0600

    Changed setup.sh so that it wouldn't break

--- setup.sh
+++ setup.sh
@@ -10,9 +10,24 @@ git submodule update --init --recursive
 # Make a .local/bin/ directory
 mkdir -p .local/bin/
 
-# Call another script to install LAMMPS
-# This script can be run separately to reinstall LAMMPS
-bash reinstall_lammps.sh
+# Install lammps
+cd lammps/src/
+make yes-misc
+make yes-molecule
+make yes-rigid
+make yes-user-sph
+make yes-user-byu
+nice make -j8 mpi
+
+# This deletes the files that were moved into the /src folder
+# by the command make yes-user-byu.  Don't worry, this doesn't delete the files 
+# out of the USER-BYU directory, running this command just makes it so that
+# git doesn't add the files to the repository twice
+make no-user-byu
+
+# Copy the executable
+cp lmp_mpi ../../.local/bin
+
 
 # Make a symbolic link
 cd ../../.local/bin

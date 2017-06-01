5021401cfba8636aad6247c2ff177568175323aa
Joshua Cordon <jjcordon@m8int01.fsl.byu.edu>
2017-06-01 09:48:29 -0600

    updated slicer.py

---
+++ devel/Joshua/slicing_simulation/file_practice.py
@@ -0,0 +1,12 @@
+import numpy as np 
+import math, random, copy
+
+with open(test.data,w) as piece:
+
+	piece.write("Hello World")
+	piece.write("This is our new text file")
+	piece.write("and this is another line.")
+	piece.write("Why? Because we can.")
+
+	piece.close()
+

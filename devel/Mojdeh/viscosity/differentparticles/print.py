a432b95c3f4bc0edfb37a325087f6fca07ddf8ba
mnikpour <mnikpour@m8int02.fsl.byu.edu>
2017-06-06 13:58:05 -0600

    exctracting information to plot

--- devel/Mojdeh/viscosity/differentparticles/print.py
+++ devel/Mojdeh/viscosity/differentparticles/print.py
@@ -12,6 +12,6 @@ with open("log.lammps",'r') as infile:  # open  this file and call it infile
 				if len(line.split()) == 11: # look for the lines with 11 items
 					outfile.write(line.split()[4] + "\n")
 			# Starting condition		
-			if re.match("^Step Temp Press Pxy visc vave pave xyrate PotEng KinEng TotEng\s*\n",line):
+			if re.match("^Step Temp Press Pxy v_visc f_vave f_pave v_xyrate PotEng KinEng TotEng\s*\n",line):
 				saveLine = True
 		

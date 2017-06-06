4e85c787d5da321b4e5210b4e0834a7e3d80e22c
mnikpour <mnikpour@m8int02.fsl.byu.edu>
2017-06-06 14:04:05 -0600

    fixed the problem with not printing in separate lines

--- devel/Mojdeh/calendaring/python_building/calendar.py
+++ devel/Mojdeh/calendaring/python_building/calendar.py
@@ -3,7 +3,7 @@ import math, random, copy, re
 from shutil import copyfile 
 
 #data string containing molecule ID, type, walldia, rho, x, y, z, 0 0 0
-wallStr = "%d %d %d 0.93 0.0 1.0 %f %f %f/n" #string for cbd particles
+wallStr = "%d %d %d 0.93 0.0 1.0 %f %f %f\n" #string for cbd particles
 
 
 walldia = 2
@@ -12,9 +12,7 @@ molID = 0
 wall_type = 4
 
 def appendLine(wallid,atomType,x,y,z):
-#	
 	linesToWrite.append(wallStr % (wallid,molID,atomType,x,y,z))
-
 
 readinFile = "sliced.data"
 preparedFile = "ready_to_calendar.data"
@@ -25,67 +23,64 @@ with open(readinFile,"r") as inFile:
 	with open(preparedFile,'w') as outFile:
 
 		foundAtoms = False
+		copyatoms = False
+		buildwall = False
 		atomCount = 0
 		linesToWrite = []
 		lines = inFile.readlines()
-		buildwall = False
-		#print (line)
+		
+
 		for line in lines:
 
 			if foundAtoms == False:
 				linesToWrite.append(line)
-				if re.match("^[ \t]*Atoms.*\n",line): #  [ \t]* means whatever number of characters before the t in this case it is a space
-				#.* means whatever that comes after atoms
+				if re.match('^[ \t]*Atoms.*\n\n',line):
 					foundAtoms = True
-					linesToWrite.append("\n")
+					copyatoms = True
+					
+			elif copyatoms == True:
+				if len(line.split()) < 4:
+					copyatoms = False
+					buildwall = True
+				else :
+					ID += 1
+					linesToWrite.append(line)
+				
 
-			else:
+			elif buildwall == True:
 				xline = linesToWrite[5]
 				yline = linesToWrite[6]
 				zline = linesToWrite[7]
+				
 				xhi = float(xline.split()[1])
 				yhi = float(yline.split()[1])
 				ylo = float(yline.split()[0])
+				
 				plateheight = float(zline.split()[1]) - walldia
 				platelen = float(xhi)
+				
 				vertex1 = (0,plateheight)
 				vertex2 = (platelen,plateheight)
-				buildwall = True
 				
-			
 				(x,z)   = vertex1
 				(x2,z2) = vertex2
 				atomType = wall_type
 				molID += 1
-				numofwallatoms = 0
 				print("molID for wall is:", molID)
 				numpoints = math.ceil(platelen/walldia)
 				delta_x = (x2-x)/numpoints
+				
 				for i in range(numpoints):
-					numofwallatoms += 1
 					xi = x + i*delta_x
 					zi = z
 					y1 = float(yhi) - walldia/2
-
 					for yi in np.arange(ylo,yhi,walldia):
 						ID += 1
 						appendLine(ID,atomType,xi,yi,zi)
-						
-				atomCount = numofwallatoms
-				if re.match('^[ \t]*Atoms.*\n*\n',line):
-					atomCount += 1
-					values = line.split()
-					values[0] = str(atomCount)
-					currentline = ""
-					for value in values:
-						currentline += value + " "
-					currentline = currentline[:-1] + "\n"
-					linesToWrite.append(currentline)
-					if re.match('^[ \t]*Velocities.*\n*\n',line):
-						break
+						buildwall = False
 
 		# Go back and correct number of atoms
-		linesToWrite[2] = str(atomCount) + " atoms\n"
+		linesToWrite[1] = str(ID) + " atoms\n"
 
 		for line in linesToWrite:
 			outFile.write(line)
@@ -109,3 +104,4 @@ print ("writing new file", preparedFile)
 
 
 
+		 

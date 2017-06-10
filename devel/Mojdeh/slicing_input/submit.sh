ec81d042e7f32093d29fa589d534461794c67729
mnikpour <mnikpour@m8int02.fsl.byu.edu>
2017-06-09 20:00:20 -0600

    fixing the half length code

--- devel/Mojdeh/slicing_input/submit.sh
+++ devel/Mojdeh/slicing_input/submit.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=12   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem=4096M   # memory per CPU core
-#SBATCH -J "halflengthCoating"   # job name
+#SBATCH -J "evapslc"   # job name
 #MACHINEFILE=`/fslapps/fslutils/generate_pbs_nodefile`
 
 # Compatibility variables for PBS. Delete if not needed.
@@ -16,7 +16,7 @@ export PBS_QUEUE=batch
 # Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
 export OMP_NUM_THREADS=1
 
-module load python/3/4
+#module load python/3/4
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 
@@ -44,4 +44,4 @@ module load python/3/4
 #mpirun -np 16 lammps -in in.granular
 #lammps -in in.granular
 # mpirun -np $SLURM_NTASKS /fslgroup/fslg_bmr_wheeler/lammps-30Jul16/src/lmp_mpi -in in.drop
-time python test.py && mpirun -np $SLURM_NTASKS lammps -in test.in
+mpirun -np $SLURM_NTASKS lammps -in evaprate_slice.in

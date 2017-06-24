da703cfb3abfd5c3e2df3ee970b62e3fb67fa382
mnikpour <mnikpour@m7int02.fsl.byu.edu>
2017-06-24 16:54:49 -0600

    getting the calender code to work with the dreid piece data file

--- devel/Mojdeh/final/calendering/calendering.py
+++ devel/Mojdeh/final/calendering/calendering.py
@@ -4,18 +4,19 @@ from shutil import copyfile
 
 #data string containing molecule ID, type, walldia, rho, x, y, z, 0 0 0
 wallStr = "%d %d %d 0.93 0.0 1.0 %f %f %f\n" #string for cbd particles
+#wallStr = "%d %d %d %f %f %f\n"
 
-
+atomsline = 2
 walldia = 1
 ID = 0
-wallmolID = 2
-wall_type = 4
+wallmolID = 200
+wall_type = 5
 
 def appendLine(wallid,atomType,x,y,z):
 	linesToWrite.append(wallStr % (wallid,molID,atomType,x,y,z))
 
 readinFile = "dried_piece.data"
-preparedFile = "calendaring.data"
+preparedFile = "calendering.data"
 
 copyfile(readinFile,preparedFile)
 
@@ -24,19 +25,20 @@ with open(readinFile,"r") as inFile:
 
 		foundAtoms = False
 		buildwall = False
-		atomCount = 0
 		linesToWrite = []
 		lines = inFile.readlines()
 		
 		for line in lines:
 			linesToWrite.append(line)
 			buildwall = True
-					
-		atoms = linesToWrite[2]
-		ID = float(atoms.split()[0])
-		#lastline = linesToWrite[-1]
-		#linesToWrite.remove(lastline)
+			if re.match('^Velocities.*\n',line):
+				linesToWrite.pop(-1)
+				linesToWrite.pop(-1)
+				break
 
+		atoms = linesToWrite[atomsline]
+		atomCount = float(atoms.split()[0])
+		ID = 10000
 		if buildwall == True:
 			xline = linesToWrite[5]
 			yline = linesToWrite[6]
@@ -66,11 +68,12 @@ with open(readinFile,"r") as inFile:
 				y1 = float(yhi) - walldia/2
 				for yi in np.arange(ylo,yhi,walldia):
 					ID += 1
+					atomCount += 1
 					appendLine(ID,atomType,xi,yi,zi)
 					buildwall = False
 
 		# Go back and correct number of atoms
-		linesToWrite[2] = str(int(ID)) + " atoms\n"
+		linesToWrite[atomsline] = str(int(atomCount)) + " atoms\n"
 
 		for line in linesToWrite:
 			outFile.write(line)

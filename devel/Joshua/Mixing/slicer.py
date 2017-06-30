618ea6216c383a5e7006b23e54e1f1afbacfbda4
Joshua Cordon <jjcordon@m8int01.fsl.byu.edu>
2017-06-30 14:17:53 -0600

    changed h values to create different sized atoms

---
+++ devel/Joshua/Mixing/slicer.py
@@ -0,0 +1,58 @@
+import numpy as np 
+import math, random, copy, re
+from shutil import copyfile 
+
+# The left and right (x) boundaries of the slice
+xlo = 20
+xhigh = 30
+dia = 2
+
+readinFile = "finished_coat.data"
+slicedFile = "sliced_coat.data"
+
+copyfile(readinFile,slicedFile)
+
+with open(readinFile,"r") as inFile:
+	with open(slicedFile,'w') as outFile:
+		
+		foundAtoms = False
+		atomCount = 0
+		linesToWrite = []
+		lines = inFile.readlines()
+		for line in lines:
+			if foundAtoms == False:
+				linesToWrite.append(line)
+				if re.match('^[ \t]*Atoms.*\n',line):
+					foundAtoms = True
+					linesToWrite.append("\n")		
+			elif len(line.split()) > 4:
+				xpos = float(line.split()[-3])
+				if xpos >= xlo and xpos <= xhigh:
+					atomCount += 1
+					# Edit the line
+					values = line.split()
+					adjustedXPos = xpos - xlo
+					values[0] = str(atomCount)
+					values[-3] = str(adjustedXPos)
+					currentline = ""
+					for value in values:
+						currentline += value + " "
+					currentline = currentline[:-1] + "\n"	
+					linesToWrite.append(currentline)
+					
+				
+		# Go back and correct number of atoms
+		linesToWrite[2] = str(atomCount) + " atoms\n"
+
+		# Change boundaries of simulation to match the slice size
+		linesToWrite[5] = str(0.0000) + " " + str(xhigh - xlo) + " xlo xhi\n"
+
+		for line in linesToWrite:
+			outFile.write(line)
+
+	outFile.close()
+inFile.close()
+
+
+print ("writing new file", slicedFile)
+

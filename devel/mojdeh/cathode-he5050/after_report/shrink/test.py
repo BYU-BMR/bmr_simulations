cdd9dfaa6c75c11f024b7d152c75f7e4e723aff5
mnikpour <mnikpour@m8int02.fsl.byu.edu>
2017-07-20 07:19:24 -0600

    drying he5050

--- devel/mojdeh/cathode-he5050/after_report/shrink/test.py
+++ devel/mojdeh/cathode-he5050/after_report/shrink/test.py
@@ -1,4 +1,4 @@
- #Python code to extract equilibrium drop coordinates and add the dr blade
+#Python code to extract equilibrium drop coordinates and add the dr blade
 import numpy as np
 #from PIL import Image
 import random, math, copy
@@ -76,7 +76,6 @@ class DatafileGenerator():
         #self.fillCubeWithCBDVtxs(vertex5,vertex6,checkForOverlap=False)
 
 
-
     def fillCubeWithRandomMixVtxs(self,v1,v2):
         (x1,z1) = v1
         (x2,z2) = v2
@@ -91,18 +90,18 @@ class DatafileGenerator():
         zh = max(z,z2)
         radius = self.act_dia/2
         for yi in np.arange(yl+self.dia*3,yh-self.dia,self.dia*7):
-            for zi in np.arange(zl-self.dia*5,zh-self.dia*7,self.dia*7):
+            for zi in np.arange(zl--self.dia*5,zh-self.dia*7,self.dia*7):
                 for xi in np.arange(xl+self.dia*3,xh-self.dia*2,self.dia*7):
-                    val = random.randint(0,130)
+                    val = random.randint(0,150)
                     if val == 0:
                         self.activecount += 1
                         self.drawRaspberry(xi,yi,zi,radius)
                         
-                    elif val >= 1 and val < 23:
+                    elif val >= 1 and val < 40:
                         self.cbdcount += 1
                         atom_type = self.cbd_type
                         self.appendLine(atom_type,xi+self.dia*5/2,yi+self.dia/2,zi+self.dia/2)
-                    elif val >= 23 and val < 131:
+                    elif val >= 40 and val < 151:
                         self.solventcount += 1
                         atom_type = self.solvent_type
                         self.appendLine(atom_type,xi+self.dia*1/2,yi+self.dia/2,zi+self.dia/2)
@@ -110,12 +109,12 @@ class DatafileGenerator():
 
 
 
-    '''def fillCubeWithActiveVtxs(self,v1,v2):
+    def fillCubeWithActiveVtxs(self,v1,v2):
         (x1,z1) = v1
         (x2,z2) = v2
         self.fillCubeWithActive(x1,self.y0,z1,x2,self.y1,z2)
         #self.fillCubeWithActive2(x1,self.y0,z1,x2,self.y1,z2)
-        #self.fillCubeWithActive3(x1,self.y0,z1,x2,self.y1,z2)'''
+        #self.fillCubeWithActive3(x1,self.y0,z1,x2,self.y1,z2)
 
     def fillCubeWithActive1(self,x,y,z,x2,y2,z2):
         xl = min(x,x2)
@@ -257,10 +256,10 @@ class DatafileGenerator():
         self.ID += 1
         self.positionLines.append(self.cbdStr % (self.ID, self.molID, atomType, x, y, z))
 
-    '''def drawWallFromVtxs(self,vtx1,vtx2,atomType=wall_type):
+    def drawWallFromVtxs(self,vtx1,vtx2,atomType=wall_type):
         (x,z)   = vtx1
         (x2,z2) = vtx2
-        self.drawWall(x,z,x2,z2,atomType)'''
+        self.drawWall(x,z,x2,z2,atomType)
 
     def drawWall(self,x,z,x2,z2,atomType=wall_type):
         self.molID += 1
@@ -274,7 +273,6 @@ class DatafileGenerator():
             zi = z+i*delta_z/2
             for yi in np.arange(self.y0,self.y1,self.dia*1):
                 self.appendLine(atomType,xi,yi,zi)
-
 
     def drawChickenNugget(self,x,y,z,rx,ry,rz,file):
         self.molID += 1
